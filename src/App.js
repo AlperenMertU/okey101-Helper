13a3f6512062c10a27dd241b9cf75f077c917e12
Alperen M. Urdoğan <89020497+AlperenMertU@users.noreply.github.com>
2023-08-11 01:08:18 +0300

    coding style

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import app from "./scss/app.css"; 
+import Pick from "./components/Pick"
+function App() {
+  return (
+    <div className="App">
+    
+      <Pick></Pick>
+
+     </div>
+  );
+}
+
+export default App;
