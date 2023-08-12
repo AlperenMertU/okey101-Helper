c6faba4b93b53ad89178bd50f60b1f8fca7c16d2
Alperen M. Urdoğan <89020497+AlperenMertU@users.noreply.github.com>
2023-08-12 12:38:12 +0300

    adding useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import app from "./scss/app.css"; 
 import Pick from "./components/Pick"
+import Takoz from "./components/Takoz"
+
 function App() {
   return (
     <div className="App">
-    
       <Pick></Pick>
-
+      <Takoz></Takoz>
      </div>
   );
 }
--- src/components/Pick.js
+++ src/components/Pick.js
@@ -2,29 +2,26 @@ import React, { useRef, useState } from 'react'
 import "../scss/pick.css";
 
 function Pick() {
-    const myDiv = useRef(null)
-    const [selectedCards, setSelectedCards] = useState([]); // Tıklanan kartları saklamak için state
-    const [sequentialSets, setSequentialSets] = useState([]); // Uygun sıralı dizileri saklamak için state
+    const [card, setCards] = useState([]);
 
-    // Tıklanan kartları işle
-    const handleCardClick = (card) => {
-        setSelectedCards(prevSelected => [...prevSelected, card]);
-        // Burada uygun sıralı dizileri kontrol edebilirsiniz ve setSequentialSets ile güncelleyebilirsiniz.
-    }
+    const handleClick = (cardId) => {
+        setCards(selected => [...selected, cardId]);
+    };
 
+    console.log(card);
 
     return (
         <div id='container'>
             <div id="pickArea">
                 <div id="gameItems">
-                    <div id="stamp-1" className="stamp blue">1</div>
-                    <div id="stamp-2" className="stamp blue">2</div>
+                    <div id="stamp-1" className="stamp blue" onClick={() => handleClick("stamp.blue")}>1</div>
+                    <div id="stamp-2" className="stamp blue" >2</div>
                     <div id="stamp-3" className="stamp blue">3</div>
                     <div id="stamp-4" className="stamp blue">4</div>
                     <div id="stamp-5" className="stamp blue">5</div>
                     <div id="stamp-6" className="stamp blue">6</div>
                     <div id="stamp-7" className="stamp blue">7</div>
-                    <div id="stamp-8" className="stamp red">1</div>
+                    <div id="stamp-8" className="stamp red" onClick={() => handleClick("stamp-red")}>1</div>
                     <div id="stamp-9" className="stamp red">2</div>
                     <div id="stamp-10" className="stamp red">3</div>
                     <div id="stamp-11" className="stamp red">4</div>
@@ -47,7 +44,10 @@ function Pick() {
                     <div id="stamp-28" className="stamp black">7</div>
                 </div>
             </div>
+         
+      <div className={card}>1</div>
         </div>
+
     )
 }
 
