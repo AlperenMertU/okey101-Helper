e32660aed502a63a31fa97f1fa9bd744343d7ba0
Alperen M. Urdoğan <89020497+AlperenMertU@users.noreply.github.com>
2023-08-23 00:06:02 +0300

    groped and sorted to numbers

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const tiles = [
   [
     { number: 1, color: 'siyah' },
     { number: 2, color: 'siyah' },
-    { number: 6, color: 'siyah' },
-    { number: 7, color: 'siyah' },
-    { number: 9, color: 'siyah' },
+    { number: 3, color: 'siyah' },
+    { number: 4, color: 'siyah' },
+    { number: 5, color: 'siyah' },
   ],
-
   [
     { number: 1, color: 'mavi' },
     { number: 3, color: 'mavi' },
@@ -44,6 +43,37 @@ yellows.sort((a, b) => a.number - b.number);
 reds.sort((a, b) => a.number - b.number);
 
 
+function groupConsecutive(arr, minCount) {
+  const result = [];
+  let consecutiveGroup = [];
+
+  for (const item of arr) {
+    if (consecutiveGroup.length === 0 || item.number === consecutiveGroup[consecutiveGroup.length - 1].number + 1) {
+      consecutiveGroup.push(item);
+    } else {
+      if (consecutiveGroup.length >= minCount) {
+        result.push(consecutiveGroup);
+      } else {
+        result.push(...consecutiveGroup);
+      }
+      consecutiveGroup = [item];
+    }
+  }
+
+  if (consecutiveGroup.length >= minCount) {
+    result.push(consecutiveGroup);
+  } else {
+    result.push(...consecutiveGroup);
+  }
+
+  return result;
+}
 
 
-console.log(darks, blues, yellows, reds);
+const darkGrouped = groupConsecutive(darks, 3);
+const yellowGrouped = groupConsecutive(yellows, 3);
+const blueGrouped = groupConsecutive(blues, 3);
+const redGrouped = groupConsecutive(reds, 3);
+
+
+console.log(yellowGrouped, darkGrouped, blueGrouped, redGrouped);
