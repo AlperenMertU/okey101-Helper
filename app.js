7f5f49580ae6752bf4a1993ca3bcaa25a6d282c6
Alperen M. Urdoğan <89020497+AlperenMertU@users.noreply.github.com>
2023-08-31 20:10:18 +0300

    ı will back later. ı hope...

--- app.js
+++ app.js
@@ -1,46 +1,67 @@
 
 const tiles = [
-  [
-    { number: 1, color: 'siyah' },
-    { number: 2, color: 'siyah' },
-    { number: 3, color: 'siyah' },
-    { number: 4, color: 'siyah' },
-    { number: 5, color: 'siyah' },
+  [ 
+    [
+      { number: 1, color: 'siyah' },
+      { number: 2, color: 'siyah' },
+      { number: 3, color: 'siyah' },
+      { number: 4, color: 'siyah' },
+      { number: 5, color: 'siyah' },
+
+    ],
+ 
   ],
   [
-    { number: 1, color: 'mavi' },
-    { number: 3, color: 'mavi' },
-    { number: 10, color: 'mavi' },
-    { number: 12, color: 'mavi' },
-    { number: 13, color: 'mavi' },
+    [
+      { number: 11, color: 'mavi' },
+      { number: 12, color: 'mavi' },
+      { number: 13, color: 'mavi' },
+    ],
+    { number: 6, color: 'mavi' },
+    { number: 8, color: 'mavi' },
+   
   ],
   [
-    { number: 7, color: 'sarı' },
-    { number: 7, color: 'sarı' },
-    { number: 1, color: 'sarı' },
-    { number: 2, color: 'sarı' },
-    { number: 3, color: 'sarı' },
-    { number: 4, color: 'sarı' },
+    [
+      { number: 1, color: 'sarı' },
+      { number: 2, color: 'sarı' },
+      { number: 3, color: 'sarı' },
+    ],
+    [
+      { number: 2, color: 'sarı' },
+      { number: 3, color: 'sarı' },
+      { number: 4, color: 'sarı' },
+    ],
   ],
   [
-    { number: 2, color: 'kırmızı' },
-    { number: 7, color: 'kırmızı' },
-    { number: 11, color: 'kırmızı' },
-    { number: 12, color: 'kırmızı' },
-    { number: 13, color: 'kırmızı' },
+    [
+      { number: 11, color: 'kırmızı' },
+      { number: 12, color: 'kırmızı' },
+      { number: 13, color: 'kırmızı' },
+    ],
+    { number: 6, color: 'kırmızı' },
+    { number: 8, color: 'kırmızı' },
   ],
 
+   [
+
+   ]
 ];
+
+function sortNumbersInTiles(tiles) {
+  for (const tileGroup of tiles) {
+    tileGroup.sort((a, b) =>  a.number - b.number);
+  }
+}
+
+sortNumbersInTiles(tiles);
 
 let darks = tiles[0]
 let blues = tiles[1]
 let yellows = tiles[2]
 let reds = tiles[3]
 
-darks.sort((a, b) => a.number - b.number);
-blues.sort((a, b) => a.number - b.number);
-yellows.sort((a, b) => a.number - b.number);
-reds.sort((a, b) => a.number - b.number);
+
 
 
 function groupConsecutive(arr, minCount) {
@@ -65,7 +86,6 @@ function groupConsecutive(arr, minCount) {
   } else {
     result.push(...consecutiveGroup);
   }
-
   return result;
 }
 
